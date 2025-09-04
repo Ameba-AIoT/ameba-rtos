6f56fdf4d56b93aa990093dbb6a4bf6179094db1
auto_jenkins_wlan5 <auto_jenkins_wlan5@realsil.com.cn>
2025-09-04 11:11:44 +0800

    update sdk
    sdk/component/audio version: 77b5d64
    1. [common][audio] fix policy config, default choose speaker
    2. [common][media] Add the temp file in cache manager
    3. [common][audio] support more accurate audio timestamp and latency
    sdk/loong version: ee8aa7c819
    1. [amebadplus][release] update release script
    2. [common][wlan] add wifi_usrcfg to set wifi minimum rx_pwr
    3. [amebadplus][wlan][halrf][bug fix-normal]Updata radioA R26.
    4. [common][cmake] optimize wget progress info
    5. [common][wlan] add api to config TX paras to improve TX performance
    6. [amebasmart][tick] update one tick cycle for sleep
    7. [common][wlan][bug fix] fix WPS connection issue
    8. [common][wlan:csi] Support CSI acquisition without connection
    9. [amebasmart][media] fix player uar compile and run issue
    10. [common][os] fix bug of image3 os_wrapper
    11. [common][cmake] Add support build non-app/soc libs
    12. [ameba][bt] Rename generic_le_audio_demo to le_audio_generic_demo
    13. [amebasmart][bt][ISO] fixed iso data send failed with no credits bug
    14. [amebasmart][bt][audio] avoid track buffer size uint32_t overflow
    15. [common][bt] refine gatts service number
    16. [amebasmart][bt:ble] Add API to get gatts attribute handle
    17. [amebasmart][bt]_Modify SBC encode frame num config and Modify HFP max data recv length_
    18. [amebasmart][bt][le audio] update A2DP sink BIS source releated demo readme
    19. [amebasmart][bt][le audio] increase mixed mode audio track buffer size
    20. [common][wakepin] Fixed the issue of incorrectly turning off wakepin event during wakepin init
    21. [amebalite][regu] set ocp of regu in sleep mode
    22. [common][wlan][bug]fix memory leak when spi block
    23. [amebadplus][ldo] close core ldo dummy load in sleep and deepsleep mode
    24. [common][wlan]add isbusidle api for spi bridge
    25. [common][cmake] modify ameba_add_subdirectory_if_exist
    26. [common][sdio][sdh] move extern statements
    27. [common][wlan][bug fix]modify set edca parameters
    28. [common][network] fix the bug of getting stuck when websocket connection
    tools/ameba version: 89923f1
    1. [tools][ImageTool]update changelog for 2.8.10
    2. [tools][TraceToo] Update TraceTool to v2.1.38
    tools/profiles/ImageTool version: 5374273
    1. [tools][floader] update flashloaders
    tools/profiles/meta_tools version: 5374273
    1. [tools][floader] update flashloaders
    tools version: 612ebe3
    1. [script][axf2bin] Fix manifest parse problem
    2. [tools]update requirements.txt

--- component/soc/usrcfg/amebadplus/ameba_wificfg.c
+++ component/soc/usrcfg/amebadplus/ameba_wificfg.c
@@ -97,6 +97,7 @@ _WEAK void wifi_set_user_config(void)
 	wifi_user_config.tx_shortcut_enable = 1;
 	wifi_user_config.rx_shortcut_enable = 1;
 	wifi_user_config.keepalive_interval = 20;
+	wifi_user_config.rx_cca_thresh = 0;
 
 	/* WPS */
 	wifi_user_config.wps_retry_count = 4;
