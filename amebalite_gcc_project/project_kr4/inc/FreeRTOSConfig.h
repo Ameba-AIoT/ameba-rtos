42616a12931813704cf30e9b0556a1d32fa87b19
auto_jenkins_wlan5 <auto_jenkins_wlan5@realsil.com.cn>
2026-01-19 03:53:08 +0800

    update sdk
    sdk/loong version: f47a65ac32
    1. [common][os] add xTimerPendFunctionCall wrapper function
    2. [amebadplus][wlan:csi] fix compatibility issues with different Cut
    3. [common][wlan][bug fix] Fix soft ap mode rx reordering issue
    4. [common][wlan] support implicit substitution of Cut version - rf
    5. [common][wlan] support implicit substitution of Cut version - bb
    6. [common][wlan] support implicit substitution of Cut version - driver
    7. [amebalite][os] enable freertos INCLUDE_xTimerPendFunctionCall
    8. [AmebaSmart][wlan][halrf][bug fix] modify halrf code related to the IC cut version
    9. [common][wlan] Close defrag err log.
    10. [amebasmart][os] update ca32 arch timer clock src
    11. [amebasmart][wlan][bug fix] add rfk flag reset flow

--- amebalite_gcc_project/project_kr4/inc/FreeRTOSConfig.h
+++ amebalite_gcc_project/project_kr4/inc/FreeRTOSConfig.h
@@ -105,6 +105,7 @@
 #define INCLUDE_vTaskDelay								1
 #define INCLUDE_xTaskGetCurrentTaskHandle 				1
 #define INCLUDE_xQueueGetMutexHolder 					1
+#define INCLUDE_xTimerPendFunctionCall                  1
 
 /* This demo makes use of one or more example stats formatting functions.  These
  * format the raw data provided by the uxTaskGetSystemState() function in to
