3ab7089934d5ad3935f5aa95967f4c3a738cf410
auto_jenkins_wlan5 <auto_jenkins_wlan5@realsil.com.cn>
2026-03-29 03:54:42 +0800

    update sdk
    sdk/loong version: 9a132eb22c
    1. [common][wlan] optimize wpa/wpa2 enterprise mapping
    2. [common][usb]fix usb suspend cmd err
    3. [common][usb]fix invalid autucal_len
    4. [common][atcmd] merge fix network atcmd issue
    5. [common][unity] add websocket client api unity test cases
    6. [common][example] autotest related changes
    7. [common][ota] delete ota header file inclusion for dplus/green2/smart in their soc header files
    8. [common][wlan]Fix longrun connect fail for wpa3
    9. [common][wlan]Add test cmd for mbo logo test 5.2.8
    10. [common][wlan]Fix mbo 5.2.3 beacon report test fail
    11. [common][eco:matter][bt] fix ble matter adapter
    12. [common][example] add ATS home appliance keep-alive test example
    13. [common][unity] correct linker section for Unity test table to ensure execution
    14. [common][unity] add httpc api unity test cases
    tools/ameba version: 9183ca5
    1. [common][tools][mcp] add ai-agent mcp flash monitor tools
    tools version: 360e587
    1. [common][tools][release] add matter ble for release

--- component/soc/amebalite/fwlib/include/section_config.h
+++ component/soc/amebalite/fwlib/include/section_config.h
@@ -22,7 +22,7 @@ extern "C" {
 #define CMD_TABLE_DATA_SECTION				SECTION(".cmd.table.data")
 #define GIT_VER_TABLE_SECTION				SECTION(".git.ver.data")
 #define IPC_TABLE_DATA_SECTION				SECTION(".ipc.table.data")
-#define UNITY_TABLE_DATA_SECTION				SECTION(".rodata.unity.table")
+#define UNITY_TABLE_DATA_SECTION				SECTION(".unity.table.rodata")
 
 /* ATCMD section type 1: used by all cores */
 #ifdef CONFIG_SUPPORT_ATCMD
