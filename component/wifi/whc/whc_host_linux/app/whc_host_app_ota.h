7ed04733b26c5312d83350d7f2ad3bcbc922979d
auto_jenkins_wlan5 <auto_jenkins_wlan5@realsil.com.cn>
2026-05-03 03:39:42 +0800

    update sdk
    sdk/loong version: f82b9629b8
    1. [RTL8730E][vfs] fix fatfs within app image error
    2. [common][wlan] fix whc host ota macro undefined error
    3. [common][wlan] fix whc host compile issue
    4. [common][wlan:whc] add whc ota part3
    5. [common][usb]tune usb drd example readme
    6. [common][wlan][bugfix]Fix NP stuck in scan state
    7. [common][fullmmac] avoid sdio fun reset in bootloader
    8. [common][wlan:whc] add whc ota part2
    9. [common][wlan:whc] add whc ota part1
    10. [RTL8730E][bt][le audio] fix A2DP PBP related demo HAL bug when BIS num is 2
    11. [RTL8730E][bt][audio] optimized conner case logic when controller_free_run_clock oveflow
    12. [common][bt:mesh]update mesh ota data structure
    13. [common][bt] Remove useless BT wake Host irq register
    14. [RTL8721Dx][wlan][bt]fix two compile errors
    15. [common][usb]tune usb example readme
    16. [common][atcmd] atcmd related changes
    17. [RTL8730E][ddr] hidden low power log
    18. [common][wlan:fw][bug fix] fw use PMU_WLAN_FW_DEVICE wakelock
    19. [common][atcmd] optimize atcmd tt mode
    tools/image_scripts version: b67e22d
    tools version: 105970f

---
+++ component/wifi/whc/whc_host_linux/app/whc_host_app_ota.h
@@ -0,0 +1,84 @@
+#ifndef _WHC_HOST_APP_OTA_H_
+#define _WHC_HOST_APP_OTA_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>             /* offsetof */
+#include <stdint.h>
+#include <string.h>
+#include <linux/genetlink.h>
+
+#define WHC_OTA_START		0
+#define WHC_OTA_READ		1
+#define WHC_OTA_WRITE		2
+#define WHC_OTA_END			3
+
+/* OTA download type */
+#define OTA_USER			0
+#define OTA_HTTP			1
+#define OTA_HTTPS			2
+#define OTA_VFS				3
+
+/* ota type */
+#define OTA_FOR_NORMAL		1
+#define OTA_FOR_RMESH		2
+
+#ifndef ETH_ALEN
+#define ETH_ALEN 			6
+#endif
+struct whc_host_ota_hdr {
+	/* fix WHC_WIFI_TEST_OTA*/
+	uint8_t type;
+	uint8_t subtype;
+	int16_t len;
+};
+
+struct whc_host_ota_info {
+	uint32_t port;
+
+	uint32_t host_len;
+	uint32_t resource_len;
+
+	uint8_t ota_type;
+	uint8_t rmesh_ota_type;
+	uint8_t rsvd[2];
+
+	char *host;
+	char *resource;
+};
+
+struct whc_ota_http_redirect {
+	char *url;
+	int len;
+	uint16_t port;
+	char *host;
+	char *resource;
+};
+
+struct whc_ota_http_response {
+	uint32_t  status_code;        /* HTTP status code */
+	uint32_t  parse_status;       /* Parse status */
+	uint32_t  body_len;           /* Response body length */
+	uint32_t  header_len;         /* Response header length */
+	uint8_t  *header_bak;         /* Response header backup */
+	uint8_t  *body;               /* Response body pointer */
+};
+
+struct whc_ota_context {
+	char *host;
+	int port;
+	char *resource;
+	char *ca_cert;
+	char *client_cert;
+	char *private_key;
+	int fd;
+	uint8_t type;
+	int NextImgLen;
+	char *NextImgBuf;
+};
+
+int whc_host_ota(void);
+void whc_host_ota_from_dev(uint8_t *buf);
+void whc_host_rmesh_ota_sock_hdl(struct msgtemplate *msg, uint32_t recv_len);
+
+#endif
