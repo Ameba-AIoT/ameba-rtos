15fc37dbea87dacfad72071c4e8419a69445cdf2
auto_jenkins_wlan5 <auto_jenkins_wlan5@realsil.com.cn>
2026-05-07 16:03:42 +0800

    update sdk
    sdk/loong version: 8071f4aa6b
    1. [common][coex] update script&comment
    2. [common][coex] update h2c 0x60 v4 for write all pta table in wl fw
    3. [common][wlan:fw] fix tdma enable cause too long wlslot part1
    4. [RTL8721F] use rtos_delay_ms as delay stub
    5. [common][wlan][bug fix] Prevent IPC queue flood from duplicate SAE auth frames
    6. [common][coex] revise ra mask filter h2c default value
    7. [common][coex] revise 778 in wl slot when driver control tdma
    8. [common][coex] revise bt state high duty init scan
    9. [common][coex] add debug port init setting for debug
    10. [RTL8721F][wlan:whc] add whc usb NIC/IOT pid
    11. [common][coex] add pta debug port init
    12. [common][coex] fix beacon interval is 0 when first wl connect
    13. [common][coex] fix gnt_bt is 0 and gnt_wl is 1 when wl rfk
    14. [common][coex] reset bt_high_rx_delta to 0 when bt high rx cnt is same
    15. [RTL8721Dx][wlan:fw][bug fix] modify 5G tickless hioe oversize error
    16. [common][coex] skip duplicate h2c 0x60 to WL FW and notify WL FW not set 778 when driver control
    17. [common][coex] fix wait_for_eapol bit not clear when softap disable
    18. [RTL8720E][RTL8721F][RTL8721Dx][wlan:fw] support tickless + tdma
    19. [common][wlan:fw][bug fix] tdma wifi lps ping AP loss 10%
    20. [common][wlan:fw] donot change 778 when value is 0
    21. [common][wlan:fw][bug fix] tdma off txpause release
    22. [common][coex] add wl rfk gpio debug pin
    23. [common][coex] add special pkt in wl state for wl connect fail when ble link busy
    24. [common][wlan:fw][bug fix] send null immediatly at tdma enable
    25. [RTL8730E][wlan:fw] support tickless + tdma
    26. [common][hal] rm mbed enums
    27. [RTL8730E][ui] do not compile ui in hp
    28. [RTL8730E][vfs] fix fatfs within app image error
    29. [common][wlan] fix whc host ota macro undefined error
    30. [common][wlan] fix whc host compile issue
    31. [common][wlan:whc] add whc ota part3
    32. [common][usb]tune usb drd example readme
    33. [common][wlan][bugfix]Fix NP stuck in scan state
    34. [common][fullmmac] avoid sdio fun reset in bootloader
    35. [common][wlan:whc] add whc ota part2
    36. [common][wlan:whc] add whc ota part1
    37. [RTL8730E][bt][le audio] fix A2DP PBP related demo HAL bug when BIS num is 2
    38. [RTL8730E][bt][audio] optimized conner case logic when controller_free_run_clock oveflow
    39. [common][bt:mesh]update mesh ota data structure
    40. [common][bt] Remove useless BT wake Host irq register
    41. [RTL8721Dx][wlan][bt]fix two compile errors
    42. [common][usb]tune usb example readme
    43. [common][atcmd] atcmd related changes
    44. [RTL8730E][ddr] hidden low power log
    45. [common][wlan:fw][bug fix] fw use PMU_WLAN_FW_DEVICE wakelock
    46. [common][atcmd] optimize atcmd tt mode
    tools/image_scripts version: b67e22d
    tools version: 5d12d18
    1. [common][tools] fix release error

--- component/wifi/whc/whc_host_threadx_fullhan/sdio/whc_sdio_host_app.c
+++ component/wifi/whc/whc_host_threadx_fullhan/sdio/whc_sdio_host_app.c
@@ -23,9 +23,14 @@ extern struct whc_sdio whc_sdio_priv;
 #define WHC_WIFI_TEST_DHCP           0x8
 #define WHC_WIFI_TEST_WIFION         0x9
 #define WHC_WIFI_TEST_SCAN_RESULT    0xA
-#define WHC_WIFI_TEST_DISCONN        0x10
+#define WHC_WIFI_TEST_MP             0xB
+#define WHC_WIFI_TEST_DBG            0xC
+#define WHC_WIFI_TEST_OTA            0x10
+/* used in fullhan now */
 #define WHC_WIFI_TEST_SOFTAP         0x11
 #define WHC_WIFI_TEST_CONN_STATUS    0x12
+#define WHC_WIFI_TEST_DISCONN        0x13
+#define WHC_WIFI_TEST_WIFIOFF        0x14
 
 /* for rtos host only */
 #define WHC_WIFI_TEST_SET_HOST_RTOS  0xFF
